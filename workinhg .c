f4f0bdca2ee59cb985d3ea22de31fd63ef131dab
ZIZINGA342 <129368350+ZIZINGA342@users.noreply.github.com>
2023-05-13 10:16:50 +0300

    Add files via upload

---
+++ workinhg .c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+intmain(void){
+	double mass;
+	double gravity;
+	double work;
+	double height;
+	double time;
+	double m;
+	
+	double h;
+	double t;
+	double g;
+	mass=m;
+	double w;
+	double p;
+	height=h;
+	gravity=g;
+	work=m*g*h;
+	p=(90/100)*w;
+	double power;
+	printf("enter mass>");
+	scanf("%f",&m);
+	printf("enter gravity>");
+	scanf("%f",&gravity);
+	printf("enter height>");
+	work=m*g*h;
+	printf("enter work>");
+	scanf("%f",&work);
+	printf("enter p>");
+	scanf("%f",&p);
+	return p;
+	return 0;
+}
